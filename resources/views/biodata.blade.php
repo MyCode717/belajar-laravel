f1ff391a89c12ff42cf64fed2352885ab38edef9
Muhammad_Iqbal_Prog01 <iqbal.nursyabab@gmail.com>
2026-09-02 20:41:38 +0700

    Route dengan parameter

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman biodata {{ $N_panggilan }}</title>
+</head>
+<body>
+    <h1><STRONG>WELCOME TO BIODATA ANGGOTA KELOMPOK 6</STRONG></h1>
+
+    <p>Halo semuanya! Perkenalkan nama saya <strong>{{ $nama }}</strong> saya dari kelompok <strong>{{ $kelompok }}.</strong><br>
+     Saya sangat suka sekali <strong>{{ $kesukaan }}</strong> dan hobi saya adalah <strong>{{ $hobi }}</strong></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
 
     $nama_ketua = "Muhammad Iqbal";
-    $kelompok = 1;
+    $kelompok = 4;
 
 
     return view('home', compact('nama_ketua', 'kelompok'));
@@ -13,4 +13,47 @@ Route::get('/home', function () {
 
 Route::get('/jargon', function(){
     return view('jargon');
+});
+
+Route::get('/biodata/{biodata}', function($biodata){
+
+$nama = null;
+$N_panggilan = null;
+$kelompok = 6;
+$kesukaan = null;
+$hobi = null;
+
+switch ($biodata){
+    case "iqbal":
+        $nama = "Muhammad Iqbal Nursyabab";
+        $N_panggilan = "Iqbal";
+        $kesukaan = "Menulis cerita";
+        $hobi = "bermain badminton";
+        break;
+    case "reihan" :
+        $nama = "Reihan Agil Ihwana";
+        $N_panggilan = "Reihan";
+        $kesukaan = "Mie Ayam";
+        $hobi = "Bermain Game";
+        break;
+    case "waska" :
+        $nama = "Waska Agung Riyanto";
+        $N_panggilan = "Waska";
+        $kesukaan = "Menonton Film";
+        $hobi = "Mancing";
+        break;
+    case "anam" :
+        $nama = "Nuzullul Anam";
+        $N_panggilan = "Anam";
+        $kesukaan = "Musik";
+        $hobi = "Dengerin Musik";
+        break;
+    default :
+        $nama = "-";
+        $N_panggilan = "-";
+        $kesukaan = "-";
+        $hobi = "-";
+
+}
+    return view('biodata', compact('nama','N_panggilan','kelompok', 'kesukaan', 'hobi'));
 });
